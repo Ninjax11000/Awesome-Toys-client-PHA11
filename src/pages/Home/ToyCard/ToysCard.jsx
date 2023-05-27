78cd172d5fc92e792240c731f9b18cb1104dd7d8
Ninjax11000 <jamiljoy007@gmail.com>
2023-05-27 10:04:20 +0600

    updated shop by category

--- src/pages/Home/ToyCard/ToysCard.jsx
+++ src/pages/Home/ToyCard/ToysCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const ToysCard = ({toy}) => {
-    const {name,category}=toy;
+    const {name,price,picture,rating}=toy;
     return (
         <div className='ms-3 my-5'>
             <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <figure><img src={picture} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
-                    <p>{category}</p>
+                    <p>Price: {price}</p>
+                    <p>Rating: {rating}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary">View Details</button>
                     </div>
                 </div>
             </div>
